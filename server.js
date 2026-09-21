aabcd27952032bdc5cb0a14f8f5f34e74ae846c2
yougan923 <ftvyle4864@qualbo.com>
2026-09-21 12:56:58 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "public", "index.html")
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(
+    "Temporary Mail site running on port " + PORT
+  );
+});
